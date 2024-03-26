be9c342de23f02daaf94e4e2ff915cad0d889da1
Eric Stevens Jr. <ericstevensjrprofessional@outlook.com>
2024-03-26 13:24:44 -0400

    Implemented logic for encoding combinations

--- logic.py
+++ logic.py
@@ -6,3 +6,19 @@ def generateCombinations(attributes):
     combinationsList = list(map(list, allCombinations))
 
     return combinationsList
+
+
+def encodeCombinations(combinations, attributes):
+    valuePositions = {attribute: {value: idx for idx, value in enumerate(values)} for attribute, values in attributes.items()}
+    encodedObjects = []
+
+    for combination in combinations:
+        encodedObject = ''
+
+        for attribute, value in zip(attributes.keys(), combination):
+            bit = '1' if valuePositions[attribute][value] == 0 else '0'
+            encodedObject += bit
+        encodedObjects.append(encodedObject)
+
+    return encodedObjects
+
