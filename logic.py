b0f7d9cdae7822be6eb47d69426447615c2360f0
Eric Stevens Jr. <ericstevensjrprofessional@outlook.com>
2024-03-26 13:20:42 -0400

    Implemented logic for generating combinations.

--- logic.py
+++ logic.py
@@ -1,29 +1,8 @@
-from pysat.solvers import Glucose4
 import itertools
 
-def assignAttributesToVariables(attributes):
-    variableMap = {}
-    counter = 1
+def generateCombinations(attributes):
+    attributeValues = [values for values in attributes.values()]
+    allCombinations = itertools.product(*attributeValues)
+    combinationsList = list(map(list, allCombinations))
 
-    for attribute, values in attributes.items():
-        for value in values:
-            key = f"{attribute}:{value}"
-            variableMap[key] = counter
-            counter += 1
-
-    return variableMap
-
-def encodeConstraints(constraints, variableMap):
-    encodedConstraints = []
-
-    for constraint in constraints:
-        clause = []
-
-        for literal in constraint:
-            isNegated = 'NOT in literal'
-            attributeValue = literal.repalce('NOT ', '')
-            variable = variableMap[attributeValue]
-            clause.append(-variable if isNegated else variable)
-        encodeConstraints.append(clause)
-
-    return encodeConstraints
+    return combinationsList
